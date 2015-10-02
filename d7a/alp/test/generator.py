d5b227ce0a2ed55b343977a3614b34b7c5e485ab
Christophe VG <contact@christophe.vg>
2015-10-02 09:35:06 +0200

    added manual msg generation test

--- d7a/alp/test/generator.py
+++ d7a/alp/test/generator.py
@@ -5,9 +5,19 @@
 
 import unittest
 
-from pprint import pprint
+from d7a.alp.parser               import Parser
 
-from d7a.alp.parser import Parser
+from d7a.alp.command              import Command
+from d7a.alp.action               import Action
+from d7a.alp.payload              import Payload
+
+from d7a.alp.operations.responses import ReturnFileData
+from d7a.alp.operands.file        import Data, Offset
+
+from d7a.sp.status                import Status
+from d7a.sp.session               import States
+
+from d7a.tp.addressee             import Addressee
 
 class TestGenerator(unittest.TestCase):
   def setUp(self):
@@ -30,6 +40,41 @@ class TestGenerator(unittest.TestCase):
     self.assertEqual(len(bytes), len(msg))
     for i in xrange(len(msg)):
       self.assertEqual(bytes[i], msg[i])
+
+  def test_simple_return_file_data_command(self):
+    cmd = Command(
+      interface=Status(
+        state=States.DONE,
+        addressee=Addressee(id=0x0102030405)
+      ),
+      payload=Payload(
+        actions=[
+          Action(
+            operation=ReturnFileData(
+              operand=Data(
+                data=list(bytearray("Hello world")),
+                offset=Offset(id=0x51)
+              )
+            )
+          )
+        ]
+      )
+    )
+    expected = [
+      0xd7,                                           # interface start
+      0x04, 0x00, 0x00, 0x00,                         # fifo config
+      0x20,                                           # universal addr = 8 bytes
+      0x00, 0x00, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, # ID
+      0x20,                                           # action=32/ReturnFileData
+      0x51,                                           # File ID
+      0x00,                                           # offset
+      0x0b,                                           # length
+      0x48, 0x65, 0x6c, 0x6c, 0x6f,                   # Hello
+      0x20, 0x77, 0x6f, 0x72, 0x6c, 0x64              # World
+    ]
+    bytes = bytearray(cmd)
+    for i in xrange(len(expected)):
+      self.assertEqual(bytes[i], expected[i])
 
 if __name__ == '__main__':
   suite = unittest.TestLoader().loadTestsFromTestCase(TestGenerator)
