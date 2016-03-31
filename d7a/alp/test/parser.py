8fc762396b2d3045c3211c558b8d19dbcb670cba
Glenn Ergeerts <glenn.ergeerts@uantwerpen.be>
2016-03-31 09:58:50 +0200

    added tests for ALP commands containing multiple actions

--- d7a/alp/test/parser.py
+++ d7a/alp/test/parser.py
@@ -168,6 +168,56 @@ class TestParser(unittest.TestCase):
     self.assertEqual(cmds[1].payload.actions[0].operation.op, 32)
     self.assertEqual(cmds[1].payload.actions[0].operation.operand.length, 4)
 
+  def test_multiple_non_grouped_actions_in_command(self):
+    (cmds, info) = self.parser.parse([
+        0xd7,                                           # interface start
+        0x04, 0x00, 0x00, 0x00,                         # fifo config
+        0x20,                                           # addr (originally 0x00)
+        0x24, 0x8a, 0xb6, 0x01, 0x51, 0xc7, 0x96, 0x6d, # ID
+        0x20,                                           # action=32/ReturnFileData
+        0x40,                                           # File ID
+        0x00,                                           # offset
+        0x04,                                           # length
+        0x00, 0xf3, 0x00, 0x00,                         # data
+        0x20,                                           # action=32/ReturnFileData
+        0x40,                                           # File ID
+        0x00,                                           # offset
+        0x04,                                           # length
+        0x00, 0xf3, 0x00, 0x00                          # data
+    ])
+    self.assertEqual(len(cmds), 1)
+    self.assertEqual(len(cmds[0].payload.actions), 2)
+    self.assertEqual(cmds[0].payload.actions[0].operation.op, 32)
+    self.assertEqual(cmds[0].payload.actions[0].operation.operand.length, 4)
+    self.assertEqual(cmds[0].payload.actions[1].operation.op, 32)
+    self.assertEqual(cmds[0].payload.actions[1].operation.operand.length, 4)
+
+  def test_multiple_grouped_actions_in_command(self):
+    (cmds, info) = self.parser.parse([
+        0xd7,                                           # interface start
+        0x04, 0x00, 0x00, 0x00,                         # fifo config
+        0x20,                                           # addr (originally 0x00)
+        0x24, 0x8a, 0xb6, 0x01, 0x51, 0xc7, 0x96, 0x6d, # ID
+        0xA0,                                           # action=32/ReturnFileData + grouped flag
+        0x40,                                           # File ID
+        0x00,                                           # offset
+        0x04,                                           # length
+        0x00, 0xf3, 0x00, 0x00,                         # data
+        0x20,                                           # action=32/ReturnFileData
+        0x40,                                           # File ID
+        0x00,                                           # offset
+        0x04,                                           # length
+        0x00, 0xf3, 0x00, 0x00                          # data
+    ])
+    self.assertEqual(len(cmds), 1)
+    self.assertEqual(len(cmds[0].payload.actions), 2)
+    self.assertEqual(cmds[0].payload.actions[0].operation.op, 32)
+    self.assertEqual(cmds[0].payload.actions[0].operation.operand.length, 4)
+    self.assertEqual(cmds[0].payload.actions[0].group, True)
+    self.assertEqual(cmds[0].payload.actions[1].operation.op, 32)
+    self.assertEqual(cmds[0].payload.actions[1].operation.operand.length, 4)
+    self.assertEqual(cmds[0].payload.actions[0].group, False)
+
 if __name__ == '__main__':
   suite = unittest.TestLoader().loadTestsFromTestCase(TestParser)
   unittest.TextTestRunner(verbosity=2).run(suite)
