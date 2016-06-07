336c83be1787fccbbfdd4c779b09eb7fd8195422
Glenn Ergeerts <glenn.ergeerts@uantwerpen.be>
2016-06-07 15:46:51 +0200

    added another example

--- d7a/serial_console_interface/examples/generate_messages.py
+++ d7a/serial_console_interface/examples/generate_messages.py
@@ -6,21 +6,40 @@ from d7a.serial_console_interface.parser import *
 from pprint import pprint
 import binascii
 
-print("\n=== Return file data, with QoS, unicast ===\n")
-cmd = Command.create_with_return_file_data_action(
-  file_id=0x40,
-  data=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-  interface_type=InterfaceType.D7ASP,
-  interface_configuration=Configuration(
-    qos=QoS(resp_mod=QoS.RESP_MODE_ALL),
-    addressee=Addressee(id_type=IdType.UID, id=1)
+def output_serial_frame(description, command):
+  print("\n=== {0} ===\n".format(description))
+
+  serial_frame = Parser.build_serial_frame(command)
+  print("command:")
+  pprint(command.as_dict())
+  print("serial frame:")
+  print(binascii.hexlify(serial_frame).decode("ascii"))
+
+output_serial_frame(
+  "Return file data, with QoS, unicast",
+  Command.create_with_return_file_data_action(
+    file_id=0x40,
+    data=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    interface_type=InterfaceType.D7ASP,
+    interface_configuration=Configuration(
+      qos=QoS(resp_mod=QoS.RESP_MODE_ALL),
+      addressee=Addressee(
+        id_type=IdType.UID,
+        id=2656824718681607041 # TODO hex string
+      )
+    )
   )
 )
 
-serial_frame = Parser.build_serial_frame(cmd)
-
-print("command:")
-pprint(cmd.as_dict())
-print("serial frame:")
-pprint(binascii.hexlify(serial_frame).decode("ascii"))
-pprint(serial_frame)
+output_serial_frame(
+  "Return file data, with QoS, broadcast",
+  Command.create_with_return_file_data_action(
+    file_id=0x40,
+    data=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    interface_type=InterfaceType.D7ASP,
+    interface_configuration=Configuration(
+      qos=QoS(resp_mod=QoS.RESP_MODE_ALL),
+      addressee=Addressee(id_type=IdType.BCAST)
+    )
+  )
+)
