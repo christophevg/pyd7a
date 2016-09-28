ede7f4e0272d8cb8a7a7916f1f6c225ff879a3b4
Christophe VG <contact@christophe.vg>
2016-09-28 16:23:14 +0200

    fixed minimal example to work with changed API

--- examples/minimal.py
+++ examples/minimal.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env PYTHONPATH=../:. python
 
-from d7a.alp.parser import Parser
+from d7a.serial_console_interface.parser import Parser
 from pprint import pprint
 
-bytes = [
-      0xd7,                                           # interface start
-      0x04, 0x00, 0x00, 0x00,                         # fifo config
-      0x20,                                           # addr (originally 0x00)
-      0x24, 0x8a, 0xb6, 0x01, 0x51, 0xc7, 0x96, 0x6d, # ID
-      0x20,                                           # action=32/ReturnFileData
-      0x40,                                           # File ID
-      0x00,                                           # offset
-      0x04,                                           # length
-      0x00, 0xf3, 0x00, 0x00                          # data
-    ]
-
-(cmds, info) = Parser().parse(bytes)
+cmd = [
+  0x20,                                   # action=32/ReturnFileData
+  0x40,                                   # File ID
+  0x00,                                   # offset
+  0x04,                                   # length
+  0x00, 0xf3, 0x00, 0x00                  # data
+]
+    
+frame = [
+  0xC0,                                   # interface sync byte
+  0,                                      # interface version
+  len(cmd),                               # ALP cmd length
+] + cmd
+    
+(cmds, info) = Parser().parse(frame)
 
 pprint(cmds[0].as_dict())
 
